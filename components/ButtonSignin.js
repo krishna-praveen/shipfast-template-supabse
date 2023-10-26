bfe091fa88c12aa1c477a391664e0d7a168030fd
Marc Lou <marc.louvion@gmail.com>
2023-10-26 11:40:34 +0800

    (new) Show user profile picture and send user to dashboard if logged in

--- components/ButtonSignin.js
+++ components/ButtonSignin.js
@@ -1,20 +1,61 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
+import { useState, useEffect } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
+import config from "@/config";
 
-// A simple button to sign in with our providers (Google & Magic Links) on the /signin page.
-// Signin page automatically redirects user to callbackUrl (config.auth.callbackUrl) after login, which is normally a private page for users to manage their accounts.
+// A simple button to sign in with our providers (Google & Magic Links).
+// It automatically redirects user to callbackUrl (config.auth.callbackUrl) after login, which is normally a private page for users to manage their accounts.
+// If the user is already logged in, it will show their profile picture & redirect them to callbackUrl immediately.
 const ButtonSignin = ({ text = "Get started", extraStyle }) => {
   const router = useRouter();
+  const supabase = createClientComponentClient();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await supabase.auth.getUser();
+
+      setUser(data.user);
+    };
+
+    getUser();
+  }, [supabase]);
+
+  const handleClick = () => {
+    if (user) {
+      router.push(config.auth.callbackUrl);
+    } else {
+      router.push(config.auth.loginUrl);
+    }
+  };
 
   return (
     <button
       className={`btn ${extraStyle ? extraStyle : ""}`}
-      onClick={() => {
-        router.push("/signin");
-      }}
+      onClick={handleClick}
     >
-      {text}
+      {user ? (
+        <>
+          {user?.user_metadata?.avatar_url ? (
+            <img
+              src={user?.user_metadata?.avatar_url}
+              alt={user?.user_metadata?.name || "Account"}
+              className="w-6 h-6 rounded-full shrink-0"
+              referrerPolicy="no-referrer"
+            />
+          ) : (
+            <span className="w-6 h-6 bg-base-300 flex justify-center items-center rounded-full shrink-0">
+              {user?.user_metadata?.name.charAt(0) || user?.email?.charAt(0)}
+            </span>
+          )}
+          {user?.user_metadata?.name || user?.email || "Account"}
+        </>
+      ) : (
+        text
+      )}
     </button>
   );
 };
