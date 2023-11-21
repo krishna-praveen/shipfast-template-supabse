de0ff9aef6cc63587c3c18ba313f570f358e2f71
Marc Lou <marc.louvion@gmail.com>
2023-11-22 07:57:09 +0800

    (minor) link to dashboard instead of router push (it's faster)

--- components/ButtonSignin.js
+++ components/ButtonSignin.js
@@ -2,15 +2,14 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Link from "next/link";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useRouter } from "next/navigation";
 import config from "@/config";
 
 // A simple button to sign in with our providers (Google & Magic Links).
 // It automatically redirects user to callbackUrl (config.auth.callbackUrl) after login, which is normally a private page for users to manage their accounts.
 // If the user is already logged in, it will show their profile picture & redirect them to callbackUrl immediately.
 const ButtonSignin = ({ text = "Get started", extraStyle }) => {
-  const router = useRouter();
   const supabase = createClientComponentClient();
   const [user, setUser] = useState(null);
 
@@ -24,41 +23,38 @@ const ButtonSignin = ({ text = "Get started", extraStyle }) => {
     getUser();
   }, [supabase]);
 
-  const handleClick = () => {
-    if (user) {
-      router.push(config.auth.callbackUrl);
-    } else {
-      router.push(config.auth.loginUrl);
-    }
-  };
+  if (user) {
+    return (
+      <Link
+        href={config.auth.callbackUrl}
+        className={`btn ${extraStyle ? extraStyle : ""}`}
+      >
+        {user?.user_metadata?.avatar_url ? (
+          <img
+            src={user?.user_metadata?.avatar_url}
+            alt={user?.user_metadata?.name || "Account"}
+            className="w-6 h-6 rounded-full shrink-0"
+            referrerPolicy="no-referrer"
+            width={24}
+            height={24}
+          />
+        ) : (
+          <span className="w-6 h-6 bg-base-300 flex justify-center items-center rounded-full shrink-0">
+            {user?.user_metadata?.name?.charAt(0) || user?.email?.charAt(0)}
+          </span>
+        )}
+        {user?.user_metadata?.name || user?.email || "Account"}
+      </Link>
+    );
+  }
 
   return (
-    <button
+    <Link
       className={`btn ${extraStyle ? extraStyle : ""}`}
-      onClick={handleClick}
+      href={config.auth.loginUrl}
     >
-      {user ? (
-        <>
-          {user?.user_metadata?.avatar_url ? (
-            <img
-              src={user?.user_metadata?.avatar_url}
-              alt={user?.user_metadata?.name || "Account"}
-              className="w-6 h-6 rounded-full shrink-0"
-              referrerPolicy="no-referrer"
-              width={24}
-              height={24}
-            />
-          ) : (
-            <span className="w-6 h-6 bg-base-300 flex justify-center items-center rounded-full shrink-0">
-              {user?.user_metadata?.name?.charAt(0) || user?.email?.charAt(0)}
-            </span>
-          )}
-          {user?.user_metadata?.name || user?.email || "Account"}
-        </>
-      ) : (
-        text
-      )}
-    </button>
+      {text}
+    </Link>
   );
 };
 
